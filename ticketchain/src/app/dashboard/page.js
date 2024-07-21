30c53f1b47fcafe5261833750b023f9d5ce3b91e
Matthew Arenivas <mrareniv@asu.edu>
2024-07-21 13:28:50 -0700

    Your commit message

--- ticketchain/src/app/dashboard/page.js
+++ ticketchain/src/app/dashboard/page.js
@@ -183,7 +183,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-import './Sidebar.css';
+import './sidebar.css';
 
 const Sidebar = ({ isOpen, onClose }) => {
   return (
